f4ec8b94825eadf85f574b9dcf89f16dede7d25f
Irina Tsypliaeva <ira@Alexanders-Mac-mini.local>
2018-12-19 10:47:10 -0800

    Added missing files

---
+++ js/popup.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(function () {
+  var createPopup = function (node) {
+
+    var open = function () {
+      node.classList.remove('hidden');
+      node.classList.remove('visually-hidden');
+      document.addEventListener('keydown', onPopupEscPress);
+    };
+
+    var close = function () {
+      node.classList.add('hidden');
+      node.classList.add('visually-hidden');
+      document.removeEventListener('keydown', onPopupEscPress);
+    };
+
+    var onPopupEscPress = function (evt) {
+      if (evt.keyCode === window.utils.keyCodes.ESC_KEYCODE) {
+        close();
+      }
+    };
+
+    return {
+      open: open,
+      close: close
+    };
+  };
+
+  window.popup = {
+    createPopup: createPopup
+  };
+})();
